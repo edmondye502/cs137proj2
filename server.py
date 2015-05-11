33634a930e1da1706c9368177f822167538e1899
Edmond Ye <eye1@uci.edu>
2015-05-11 14:18:15 -0700

    initial commit

---
+++ server.py
@@ -0,0 +1,22 @@
+from network import Listener, Handler, poll
+
+ 
+handlers = {}  # map client handler to user name
+ 
+class MyHandler(Handler):
+     
+    def on_open(self):
+        pass
+         
+    def on_close(self):
+        pass
+     
+    def on_msg(self, msg):
+    	print('sdfsdf')
+        print msg
+ 
+
+port = 8888
+server = Listener(port, MyHandler)
+while 1:
+    poll(timeout=0.05) # in seconds
