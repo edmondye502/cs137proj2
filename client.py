94c2d3dba49e4df095513fcc3b55bf48727a15d0
meltonb <brentmelton99@yahoo.com>
2015-05-11 19:53:14 -0700

    reorganized most of code

--- client.py
+++ client.py
@@ -4,40 +4,47 @@ from threading import Thread
 from time import sleep
 
 
-myname = raw_input('What is your name? ')
-
-while (True):
-	prompt = input('1. Q \n2. C \n3. O\n Input: ')
-	if(int(prompt) <= 3 and int(prompt) >= 1):
-		break
-
-
-topic = raw_input('Topic: ')
 
 class Client(Handler):
     
     def on_close(self):
-        pass
+        print("Client now exiting!")
     
     def on_msg(self, msg):
-        print msg
+        print(msg)
+
+
         
-host, port = 'localhost', 8888
-client = Client(host, port)
-client.do_send({'join': myname})
+def prompt():
+    while (True):
+        prompt = input('(1) Question \n(2) Complaint \n(3) Other\n Input: ')
+        if(int(prompt) <= 3 and int(prompt) >= 1):
+            break
 
 def periodic_poll():
     while 1:
         poll()
         sleep(0.05)  # seconds
                             
-thread = Thread(target=periodic_poll)
-thread.daemon = True  # die when the main thread dies 
-thread.start()
 
 
-print('Connecting you with an agent')
 
-while 1:
-    mytxt = sys.stdin.readline().rstrip()
-    client.do_send({'speak': myname, 'txt': mytxt})
+
+if __name__ == '__main__':
+    myname = raw_input('What is your name? ')
+    prompt()
+    topic = raw_input('Topic: ')
+
+    host, port = 'localhost', 8888
+    client = Client(host, port)
+    client.do_send({'join': myname})
+
+    thread = Thread(target=periodic_poll)
+    thread.daemon = True  # die when the main thread dies 
+    thread.start()
+
+    done = False; 
+    while not done:
+        mytxt = sys.stdin.readline().rstrip()
+        client.do_send({'speak': myname, 'txt': mytxt})
+    client.do_close() 
--- server.py
+++ server.py
@@ -1,22 +1,24 @@
 from network import Listener, Handler, poll
 
- 
-handlers = {}  # map client handler to user name
- 
 class MyHandler(Handler):
      
     def on_open(self):
-        pass
+        print("OPENING")
          
     def on_close(self):
-        pass
+        print("CLOSING")
+
      
     def on_msg(self, msg):
-    	self.do_send(msg)
-        print msg
+    	reply = input("")
+    	# do_send(reply)
+   
+        
  
 
-port = 8888
-server = Listener(port, MyHandler)
-while 1:
-    poll(timeout=0.05) # in seconds
+if __name__ == '__main__':
+	handlers = {}  # map client handler to user name
+	port = 8888
+	server = Listener(port, MyHandler)
+	while 1:
+	    poll(timeout=0.05) # in seconds
