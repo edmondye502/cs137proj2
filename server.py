fad78f9e945dd937b29aefc297bf87e0dd2f0a65
Edmond Ye <eye1@uci.edu>
2015-05-31 19:09:23 -0700

    Brent fixed log

--- server.py
+++ server.py
@@ -7,7 +7,12 @@ class MyHandler(Handler):
 	def on_open(self):
 		print("Connection with a client has been established")
 		handlers.append(self.sock)
-		self.do_send("Connecting you with any Agent. Please wait...")
+		for i in range(len(handlers)):
+			if self.sock == handlers[i]:
+				position_num = i - 0
+				break
+
+		self.do_send("Connecting you with any Agent. Please wait... You are number " + str(position_num) + " in the queue.")
 
 	def on_close(self):
 		print("Client has disconnected...")
@@ -21,14 +26,21 @@ class MyHandler(Handler):
 				print("You are speaking with " + msg.get('name'))
 				print(msg.get('name') + ' chose ' + msg.get('prompt'))
 				print("The topic is: " + msg.get('topic'))
+
+				reply = raw_input("Reply: ")
+				self._log.append("Agent: " + reply)
+				self.do_send(reply)
 			
 			elif len(msg) == 1:
+
+				self._log.append("Client: " + msg.get('txt'))
 
 				# save log
 				if(msg.get('txt') == ":s"):
 					infile = open("log.txt", "w")
-					strlist_to_str(self._buffer)
-					infile.write()
+					log_str = strlist_to_str(self._log)
+					infile.write(log_str)
+					self.do_send("Conversation saved.")
 
 				# easter egg
 				elif(msg.get('txt') == ":e"):
@@ -37,9 +49,10 @@ class MyHandler(Handler):
 
 				else:
 					print("Client: " + msg.get('txt'))
-
-			reply = raw_input("Reply: ")
-			self.do_send(reply)
+					reply = raw_input("Reply: ")
+					self._log.append("Agent: " + reply)
+					self.do_send(reply)
+				
 
 		else:
 			self.do_send("Agent is busy")
